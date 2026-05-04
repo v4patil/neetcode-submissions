0013235f226d524e23d1c03c96975ccc819a01c2
v4patil <63712841+v4patil@users.noreply.github.com>
2026-05-04 23:36:21 +0530

    Add: anagram-groups - submission-7

---
+++ Data Structures & Algorithms/anagram-groups/submission-7.kt
@@ -0,0 +1,22 @@
+class Solution {
+    fun groupAnagrams(strs: Array<String>): List<List<String>> {
+    val res: HashMap<String, MutableList<String>> = HashMap()
+    for (str in strs) {
+        val count = IntArray(26)
+        val arr = str.toCharArray()
+        for (c in arr) {
+            count[c - 'a']++
+        }
+        //val key = count.joinToString()// java 67.7 mb and kotlin 474.8 mb reason joinToString
+        val sb = StringBuilder()
+        for (i in 0 until 26) {
+            sb.append('#')
+            sb.append(count[i])
+        }
+        val key = sb.toString()
+        res.putIfAbsent(key, ArrayList())
+        res[key]!!.add(str)
+    }
+    return res.values.toList()
+}
+}
